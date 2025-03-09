e6e5a5d44abc570f7a3f248f6e50480fdbfaf525
delphvr <delphine.vanrossum@student.uclouvain.be>
2025-03-09 20:10:06 +0100

    no two rehearsal whith two participant with same id

--- backend/src/main/java/calendarapp/cp/BasicExample.java
+++ backend/src/main/java/calendarapp/cp/BasicExample.java
@@ -1,5 +1,6 @@
 package calendarapp.cp;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -41,20 +42,52 @@ public class BasicExample {
         }
     }
 
+    private static boolean isCommunParticipant(List<Integer> participantsId1, List<Integer> participantsId2){
+        for(int id1 : participantsId1){
+            for (int id2 : participantsId2){
+                if (id1 == id2){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static List<List<IntervalVar>> rehearsalsCommunParticipant(List<Rehearsal> rehearsals, Map<Integer, IntervalVar> rehearsalIntervals){
+        List<List<IntervalVar>> res = new ArrayList<>();
+        for (int i=0; i<rehearsals.size(); i++){
+            List<IntervalVar> rehearsalsList = new ArrayList<>();
+            for (int j=i+1; j<rehearsals.size(); j++){
+                Rehearsal rehearsal1 = rehearsals.get(i);
+                Rehearsal rehearsal2 = rehearsals.get(j);
+                if(isCommunParticipant(rehearsal1.participantsId, rehearsal2.participantsId)){
+                    rehearsalsList.add(rehearsalIntervals.get(rehearsal2.id));
+                }
+            }
+            if (!rehearsalsList.isEmpty()) {
+                rehearsalsList.add(rehearsalIntervals.get(rehearsals.get(i).id));
+                res.add(rehearsalsList);
+            }
+
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         Loader.loadNativeLibraries();
 
         final List<Rehearsal> allRehearsals = Arrays.asList(
                 new Rehearsal(1, 30, Arrays.asList(1, 2, 3)),
-                new Rehearsal(2, 120, Arrays.asList(1, 5, 6, 7, 8)));
+                new Rehearsal(2, 120, Arrays.asList(1, 5, 6, 7, 8)),
+                new Rehearsal(3, 160, Arrays.asList(4)),
+                new Rehearsal(4, 160, Arrays.asList(2, 4)));
 
         CpModel model = new CpModel();
 
         Map<Integer, RehearsalSchedule> rehearsalsSchedule = new HashMap<>();
         Map<Integer, IntervalVar> rehearsalIntervals = new HashMap<>();
         for (Rehearsal rehearsal : allRehearsals) {
-            IntVar start = model.newIntVar(periode_begining, periode_end - rehearsal.duration,
-                    "start_rehearsal_" + rehearsal.id);
+            IntVar start = model.newIntVar(periode_begining, periode_end - rehearsal.duration, "start_rehearsal_" + rehearsal.id);
             IntVar end = model.newIntVar(periode_begining, periode_end, "end_rehearsal_" + rehearsal.id);
             IntervalVar interval = model.newIntervalVar(start, LinearExpr.constant(rehearsal.duration), end, "interval_rehearsal_" + rehearsal.id);
             rehearsalsSchedule.put(rehearsal.id, new RehearsalSchedule(start, end));
@@ -62,8 +95,9 @@ public class BasicExample {
         }
 
         // constraints
-        for (Rehearsal rehearsal : allRehearsals) {
-            
+        List<List<IntervalVar>> intervalsConstraintsList = rehearsalsCommunParticipant(allRehearsals, rehearsalIntervals);
+        for(List<IntervalVar> intervalsList : intervalsConstraintsList){
+            model.addNoOverlap(intervalsList);
         }
 
         CpSolver solver = new CpSolver();
